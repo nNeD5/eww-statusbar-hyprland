ee9edd813faab29abe6633ecf58b3759e95613aa
nNeD5 <oned.reg@gmail.com>
2023-08-15 16:48:35 +0300

    fix launch script

--- launch.sh
+++ launch.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-WORKDIR=$(pwd)
+WORKDIR=$(dirname "$0")
 EWW="eww -c $WORKDIR"
 
-# Open widgets
-run_eww() {
-    ${EWW} close-all
-    ${EWW} --force-wayland --restart open-many bar bar1
-}
-run_eww
+${EWW} open-many bar bar1
