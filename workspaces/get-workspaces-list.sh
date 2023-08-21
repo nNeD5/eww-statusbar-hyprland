102270c06f7926697b8ad616d392416d988e7c51
nNeD5 <oned.reg@gmail.com>
2023-08-21 19:51:07 +0300

    support specialworkspace(scratchpad)

--- workspaces/get-workspaces-list.sh
+++ workspaces/get-workspaces-list.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 
 spaces (){
     # ouput format: [1, 2, 3]
     echo -n "["
-    hyprctl workspaces | grep "ID" | cut -d" " -f3 | sort -n | sed -z 's/\n/,/g;s/,$//'
+    hyprctl workspaces | grep "ID" | cut -d" " -f3 | sort -n | sed -z 's/\n/,/g;s/,$//' | sed -r 's/-[0-9]+,//'
     echo "]"
 }
 
